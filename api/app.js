45ee388373839ab2fae93e47724fe6af19832f66
ditconhanhphuc <21522021@gm.uit.edu.vn>
2024-12-02 21:04:05 +0700

    deploy to ec2

--- api/app.js
+++ api/app.js
@@ -18,8 +18,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-    credentials: true}))
+// app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+//     credentials: true}))
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
